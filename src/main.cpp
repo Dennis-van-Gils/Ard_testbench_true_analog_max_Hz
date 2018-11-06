f21988ba9b609a1cf85ee4c45bdcd2e575441fb5
Dennis-van-Gils <vangils.dennis@gmail.com>
2018-11-06 19:18:19 +0100

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -14,30 +14,31 @@ static void syncDAC() {while (DAC->STATUS.bit.SYNCBUSY == 1);}
 #define Ser Serial
 DvG_SerialCommand sc(Ser);  // Instantiate serial command listener
 
-#define ANALOG_READ_RESOLUTION  12    // [bits] 10 or 12 for M0 Pro
 #define ANALOG_WRITE_RESOLUTION 10    // [bits] Fixed to 10 on M0 Pro
 #define INTERRUPT_CLOCK_PERIOD  50    // [usec]
 
-double sine_freq = 133.33;   // [Hz]
+double sine_freq = 200.0;   // [Hz]
 volatile uint32_t sine_out;
 
 /*------------------------------------------------------------------------------
     Sine wave look-up table (LUT)
 ------------------------------------------------------------------------------*/
 
-#define A_REF 3.300   // [V] Analog voltage reference
 #define N_LUT 2048    // Number of elements for one full period
+#define A_REF 3.300   // [V] Analog voltage reference
 
 // Tip: limiting the output voltage range to slighty above 0.0 V will improve
 // the shape of the sine wave at it's minimum. Apparently, the analog out port
 // has difficulty in cleanly dropping the output voltage completely to 0.0 V.
-const double V_p2p = 2.0;     // [V] peak to peak
-const double V_center = 2.0;  // [V]
+#define V_out_p2p    2.0      // [V] peak to peak
+#define V_out_center 2.0      // [V]
+
 uint32_t LUT_sine[N_LUT] = {0};
+volatile double LUT_micros_to_index_factor = 1e-6 * sine_freq * N_LUT;
 
 void create_LUT_sine() {
-  const double offset = V_center / A_REF;
-  const double amplitude = 0.5 / A_REF * V_p2p;
+  const double offset = V_out_center / A_REF;
+  const double amplitude = 0.5 / A_REF * V_out_p2p;
   for (int i = 0; i < N_LUT; i++) {
     LUT_sine[i] = (uint32_t) round((pow(2, ANALOG_WRITE_RESOLUTION) - 1) * \
                   (offset + amplitude * sin(2*PI*i/N_LUT)));
@@ -48,18 +49,13 @@ void create_LUT_sine() {
     Interrupt service routine
 ------------------------------------------------------------------------------*/
 
-volatile uint32_t LUT_index;
-double LUT_micros_to_index_factor = 1e-6 * sine_freq * N_LUT;
-
 void myISR() {
+  uint32_t LUT_index;
   LUT_index = ((uint32_t) round(micros() * LUT_micros_to_index_factor)) % N_LUT;
   sine_out = LUT_sine[LUT_index];
 
   syncDAC();
   DAC->DATA.reg = sine_out & 0x3FF;
-  syncDAC();
-  DAC->CTRLA.bit.ENABLE = 0x01;
-  syncDAC();
   return;
 }
 
@@ -69,9 +65,9 @@ void myISR() {
 
 void setup() {
   Ser.begin(9600);
-  analogReadResolution(ANALOG_READ_RESOLUTION);
 
   create_LUT_sine();
+  DAC->CTRLA.bit.ENABLE = 0x01;
   TC.startTimer(INTERRUPT_CLOCK_PERIOD, myISR);
 }
 
